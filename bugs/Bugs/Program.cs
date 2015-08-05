f9cd5ed58d20eaa1ba8a0c1025212aa43db8ee0b
Jared Parsons <jaredpar@microsoft.com>
2015-08-05 13:58:06 -0700

    Simple Github Client

---
+++ bugs/Bugs/Program.cs
@@ -0,0 +1,37 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugs
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Go().Wait();
+        }
+
+        private static async Task Go()
+        {
+            var githubClient = new GitHubClient(new ProductHeaderValue("Bugs"));
+
+            var colon = ':';
+            var q = $@"repo{colon}dotnet/roslyn+label{colon}Area-Compilers+milestone{colon}1.1+is{colon}open";
+            var map = new Dictionary<string, string>(StringComparer.Ordinal);
+            map[nameof(q)] = q;
+            map["page"] = "2";
+
+            var apiConnection = new ApiConnection(githubClient.Connection);
+            var apiUrl = ApiUrls.SearchIssues();
+            var html = await apiConnection.GetHtml(apiUrl, map);
+            var result = await apiConnection.Get<SearchIssuesResult>(apiUrl, map);
+            foreach (var cur in result.Items)
+            {
+                Console.WriteLine(cur.Title);
+            }
+        }
+    }
+}
