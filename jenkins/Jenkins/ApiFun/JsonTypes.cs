367640e3755f5fb5d67efff60a3e0b0def62dde8
Jared Parsons <jaredpar@microsoft.com>
2015-09-10 13:59:23 -0700

    Can query list of builds

---
+++ jenkins/Jenkins/ApiFun/JsonTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFun
+{
+    public class Build
+    {
+        public int Number { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return $"Build {Number}";
+        }
+    }
+}
---
+++ jenkins/Jenkins/ApiFun/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFun
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            GetWindowsPullJobs();
+        }
+
+        private static List<Build> GetWindowsPullJobs()
+        {
+            var client = new RestClient("http://dotnet-ci.cloudapp.net");
+            var request = new RestRequest("job/dotnet_roslyn_prtest_win/api/json", Method.GET);
+            request.AddParameter("pretty", "true");
+            var content = client.Execute(request).Content;
+            var data = JObject.Parse(content);
+            var all = (JArray)data["builds"];
+
+            var list = new List<Build>();
+            foreach (var cur in all)
+            {
+                list.Add(cur.ToObject<Build>());
+            }
+
+            return list;
+        }
+    }
+}
