88bc2f00fdc7b6fe69a2d9f37a44ea70a0ce8cb4
Jared Parsons <jaredpar@microsoft.com>
2015-10-18 12:03:42 -0700

    Stupid website

---
+++ sites/EmilyLabor/EmilyLabor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmilyLabor.Startup))]
+namespace EmilyLabor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
