7fa6718557ed77d1a7beac0c8f3227defd64a1af
Jared Parsons <jaredpar@microsoft.com>
2015-12-07 13:22:05 -0800

    Data passing

---
+++ dashboard/Dashboard/Controllers/IssuesController.cs
@@ -0,0 +1,20 @@
+﻿using Dashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class IssuesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new IssuesViewModel();
+            model.Issues.Add(new Issue() { Id = 42, Url = "blah" });
+            model.Issues.Add(new Issue() { Id = 13, Url = "blah" });
+            return View(model);
+        }
+    }
+}
---
+++ dashboard/Dashboard/Models/IssuesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard.Models
+{
+    public class IssuesViewModel
+    {
+        public List<Issue> Issues { get; } = new List<Issue>();
+    }
+
+    public sealed class Issue
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+    }
+}
