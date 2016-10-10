3810d0956138f8341209a7136c5a4269b34139b0
Jared Parsons <jaredpar@microsoft.com>
2016-10-10 12:58:24 -0700

    beginning

---
+++ dotnet/GetTempPath/GetTempPath/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTempPath
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+
+        }
+
+        private static void Test(
+            string userProfile = null,
+            string temp = null,
+            string tmp = null,
+            string 
+        {
+
+
+        }
+
+
+    }
+}
