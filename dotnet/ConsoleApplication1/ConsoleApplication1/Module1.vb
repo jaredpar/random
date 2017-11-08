059c78c449e15c726f044d429a9793b24738a91c
Jared Parsons <jaredpar@microsoft.com>
2017-11-08 11:06:43 -0800

    Test

---
+++ dotnet/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
+
+Class A
+
+    Public value As Integer = 1
+    Sub Action1(Optional val As Integer = value) ' BC30369 - WEIRD COMPILER ERROR 
+    End Sub
+
+    Public Shared sharedValue As Integer = 2
+    Sub Action2(Optional val As Integer = sharedValue) 'BC30059 - CORRECT COMPILER ERROR 
+    End Sub
+
+End Class
