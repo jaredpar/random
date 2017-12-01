31a205f209b4d9808454d4c975ebe420074eaf48
Jared Parsons <jaredpar@microsoft.com>
2017-12-01 10:33:46 -0800

    Simple tool for printing out imports in tree form

---
+++ dotnet/PrintImportTree/PrintImportTree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace PrintImportTree
+{
+    internal static class Program
+    {
+        private enum Kind
+        {
+            None,
+            ImportStart,
+            ImportEnd
+        }
+
+        internal static void Main(string[] args)
+        {
+            var file = @"e:\temp\scratch.xml";
+            var indent = 0;
+            var indentString = "";
+            foreach (var line in File.ReadAllLines(file))
+            {
+                var (kind, name) = ClassifyLine(line);
+                switch (kind)
+                {
+                    case Kind.ImportStart:
+                        Console.WriteLine($"{indentString}{name}");
+                        indent++;
+                        indentString = new string(' ', indent * 2);
+                        break;
+                    case Kind.ImportEnd:
+                        indent--;
+                        indentString = new string(' ', indent * 2);
+                        Console.WriteLine($"{indentString}{name}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static (Kind, string) ClassifyLine(string line)
+        {
+            var dirMatch = @"[\w\W\d:-\\/]+";
+            var options = RegexOptions.Compiled | RegexOptions.IgnoreCase;
+            var endMatch = Regex.Match(line, $@"^\s*<!-- ======== END OF ({dirMatch})+ ======= -->", options);
+            if (endMatch.Success)
+            {
+                return (Kind.ImportEnd, endMatch.Groups[1].Value);
+            }
+
+            var startMatch = Regex.Match(line, $@"^\s*<!-- ======== ({dirMatch}) ======= -->", options);
+            if (startMatch.Success)
+            {
+                return (Kind.ImportStart, startMatch.Groups[1].Value);
+            }
+
+            return (Kind.None, null);
+        }
+    }
+}
---
+++ dotnet/PrintImportTree/PrintImportTree/README.md
@@ -0,0 +1,1 @@
+﻿Tool for printing out the import tree for a pre-processed MSBuild file
