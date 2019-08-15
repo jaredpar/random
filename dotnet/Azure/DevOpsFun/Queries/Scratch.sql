57225eeb65b076eb744d43405573e7d962d1efa1
Jared Parsons <jaredpparsons@gmail.com>
2019-08-15 09:13:26 -0700

    Simple azure function application

--- dotnet/Azure/DevOpsFun/Queries/Scratch.sql
+++ dotnet/Azure/DevOpsFun/Queries/Scratch.sql
@@ -1,1 +1,3 @@
-﻿SELECT * FROM dbo.BuildCloneTime WHERE DefinitionId = 15 and BuildStartTime > '8/14	/2019' and maxduration > '00:10:00'
+﻿/* SELECT * FROM dbo.BuildCloneTime WHERE DefinitionId = 15 and BuildStartTime > '8/14	/2019' and maxduration > '00:10:00' */
+
+SELECT * FROM dbo.JobCloneTime WHERE BuildId = 311446
