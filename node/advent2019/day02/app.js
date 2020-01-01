1654864394f4fdfa51bf393d24b30f7e130b212a
Jared Parsons <jaredpparsons@gmail.com>
2020-01-01 15:28:45 -0800

    Solved day 02
    Solved the advent day 2 puzzle

--- node/advent2019/day02/app.js
+++ node/advent2019/day02/app.js
@@ -1,8 +1,14 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-function process(contents) {
-    var instructions = contents.split(',').map(function (c) { return parseInt(c); });
+function process(instructions) {
     var index = 0;
     while (true) {
         var instruction = instructions[index];
@@ -15,16 +21,58 @@ function process(contents) {
         var right = instructions[rightIndex];
         var destIndex = instructions[index + 3];
         if (instruction == 1) {
-            console.log("Add " + leftIndex + "(" + left + ") + " + rightIndex + "(" + right + ") into " + destIndex);
+            // console.log(`Add ${leftIndex}(${left}) + ${rightIndex}(${right}) into ${destIndex}`)
             instructions[destIndex] = left + right;
         }
         else {
-            console.log("Multiply " + leftIndex + "(" + left + ") + " + rightIndex + "(" + right + ") into " + destIndex);
+            // console.log(`Multiply ${leftIndex}(${left}) + ${rightIndex}(${right}) into ${destIndex}`)
             instructions[destIndex] = left * right;
         }
         index += 4;
     }
-    console.log(instructions[0]);
+    return instructions[0];
 }
-fs.readFile('input.txt', 'utf8', function (err, contents) { return process(contents); });
+function convertToInstructions(contents) {
+    var instructions = contents.split(',').map(function (c) { return parseInt(c); });
+    return instructions;
+}
+function processText(contents) {
+    var instructions = convertToInstructions(contents);
+    var value = process(instructions);
+    console.log(value);
+}
+function processFile() {
+    fs.readFile('input.txt', 'utf8', function (err, contents) { return processText(contents); });
+}
+function processSolver() {
+    function processInstructions(instructions) {
+        for (var i = 0; i <= 99; i++) {
+            for (var j = 0; j <= 99; j++) {
+                var copy = __spreadArrays(instructions);
+                copy[1] = i;
+                copy[2] = j;
+                var value = process(copy);
+                if (value === 19690720) {
+                    return [i, j];
+                }
+            }
+        }
+        return null;
+    }
+    fs.readFile('input.txt', 'utf8', function (err, contents) {
+        var instructions = convertToInstructions(contents);
+        var result = processInstructions(instructions);
+        if (result) {
+            var noun = result[0], verb = result[1];
+            console.log("noun: " + noun + " verb: " + verb);
+            var answer = (noun * 100) + verb;
+            console.log(answer);
+        }
+        else {
+            console.log("Could not determine the values");
+        }
+    });
+}
+processSolver();
+// processFile();
 //# sourceMappingURL=app.js.map
--- node/advent2019/day02/app.ts
+++ node/advent2019/day02/app.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs'
 
-function process(contents: string) {
-    let instructions = contents.split(',').map(c => parseInt(c));
+function process(instructions: number[]): number {
     let index = 0
     while (true) {
         let instruction = instructions[index];
@@ -15,17 +14,65 @@ function process(contents: string) {
         let right = instructions[rightIndex]
         let destIndex = instructions[index + 3]
         if (instruction == 1) {
-            console.log(`Add ${leftIndex}(${left}) + ${rightIndex}(${right}) into ${destIndex}`)
+            // console.log(`Add ${leftIndex}(${left}) + ${rightIndex}(${right}) into ${destIndex}`)
             instructions[destIndex] = left + right;
         }
         else {
-            console.log(`Multiply ${leftIndex}(${left}) + ${rightIndex}(${right}) into ${destIndex}`)
+            // console.log(`Multiply ${leftIndex}(${left}) + ${rightIndex}(${right}) into ${destIndex}`)
             instructions[destIndex] = left * right;
         }
         index += 4;
     }
 
-    console.log(instructions[0]);
+    return instructions[0];
 }
 
-fs.readFile('input.txt', 'utf8', (err, contents: string) => process(contents));
+function convertToInstructions(contents: string): number[] {
+    let instructions = contents.split(',').map(c => parseInt(c));
+    return instructions;
+}
+
+function processText(contents: string) {
+    let instructions = convertToInstructions(contents);
+    let value = process(instructions);
+    console.log(value);
+}
+
+function processFile() {
+    fs.readFile('input.txt', 'utf8', (err, contents: string) => processText(contents));
+}
+
+function processSolver() {
+    function processInstructions(instructions: number[]): [number, number] | null {
+        for (let i = 0; i <= 99; i++) {
+            for (let j = 0; j <= 99; j++) {
+                const copy = [...instructions];
+                copy[1] = i;
+                copy[2] = j;
+                const value = process(copy);
+                if (value === 19690720) {
+                    return [i, j];
+                }
+            }
+        }
+
+        return null;
+    }
+
+    fs.readFile('input.txt', 'utf8', function (err, contents: string) {
+        let instructions = convertToInstructions(contents);
+        let result = processInstructions(instructions);
+        if (result) {
+            let [noun, verb] = result
+            console.log(`noun: ${noun} verb: ${verb}`);
+            let answer = (noun * 100) + verb;
+            console.log(answer);
+        }
+        else {
+            console.log("Could not determine the values");
+        }
+    });
+}
+
+processSolver();
+// processFile();
