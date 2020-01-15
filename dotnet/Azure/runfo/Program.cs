8ddab04aa52148b5ad0ec8cde6def86fce555f9b
Jared Parsons <jaredpparsons@gmail.com>
2020-01-15 14:35:32 -0800

    Fixed the launch of runfo

--- dotnet/Azure/runfo/Program.cs
+++ dotnet/Azure/runfo/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace runfo
+public class Program
 {
-    class Program
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Hello World!");
     }
 }
