ee55f73754e3e891879080e7f12b9d2e912a88fd
Jared Parsons <jaredpparsons@gmail.com>
2020-01-24 09:22:32 -0800

    Top level command line working

--- dotnet/Azure/runfo/Program.cs
+++ dotnet/Azure/runfo/Program.cs
@@ -6,11 +6,42 @@ using DevOps.Util;
 
 public class Program
 {
+    private const int ExitSuccess = 0;
+    private const int ExitFailure = 1;
 
-    internal static async Task Main(string[] args)
+    internal static async Task<int> Main(string[] args)
     {
-        var runtimeInfo = new RuntimeInfo();
-        await runtimeInfo.PrintBuildResults();
-    }
+        try
+        {
+            var runtimeInfo = new RuntimeInfo();
+            if (args.Length == 0)
+            {
+                await runtimeInfo.PrintBuildResults();
+                return ExitSuccess;
+            }
 
+            var command = args[0].ToLower();
+            switch (command)
+            {
+                case "status":
+                    await runtimeInfo.PrintBuildResults();
+                    return ExitSuccess;
+                default:
+                    Console.WriteLine($"Error: {command} is not recognized as a valid command");
+                    ShowHelp();
+                    return ExitFailure;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return ExitFailure;
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("runfo");
+            Console.WriteLine("\tstatus\tPrint build status");
+        }
+    }
 }
